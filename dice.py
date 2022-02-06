c76350e42ea55f15c34798e392f4d127216a895d
Owen Crow <owen.crow@gmail.com>
2022-02-06 23:16:46 +0000

    Works, first try

--- dice.py
+++ dice.py
@@ -2,6 +2,75 @@
 """
 import random
 
+DICE_ART = {
+        1: (
+            "┌─────────┐",
+            "│         │",
+            "│    ●    │",
+            "│         │",
+            "└─────────┘",
+            ),
+        2: (
+            "┌─────────┐",
+            "│  ●      │",
+            "│         │",
+            "│      ●  │",
+            "└─────────┘",
+            ),
+        3: (
+            "┌─────────┐",
+            "│  ●      │",
+            "│    ●    │",
+            "│      ●  │",
+            "└─────────┘",
+            ),
+        4: (
+            "┌─────────┐",
+            "│  ●   ●  │",
+            "│         │",
+            "│  ●   ●  │",
+            "└─────────┘",
+            ),
+        5: (
+            "┌─────────┐",
+            "│  ●   ●  │",
+            "│    ●    │",
+            "│  ●   ●  │",
+            "└─────────┘",
+            ),
+        6: (
+            "┌─────────┐",
+            "│  ●   ●  │",
+            "│  ●   ●  │",
+            "│  ●   ●  │",
+            "└─────────┘",
+            ),
+        }
+DIE_HEIGHT = len(DICE_ART[1])
+DIE_WIDTH = len(DICE_ART[1][0])
+DIE_FACE_SEPARATOR = " "
+
+def generate_dice_faces_diagram(dice_values):
+    """Return an ASCII diagram of dice faces from 'dice_values'.
+    """
+    dice_faces = []
+    for value in dice_values:
+        dice_faces.append(DICE_ART[value])
+
+    dice_faces_rows = []
+    for row_idx in range(DIE_HEIGHT):
+        row_components = []
+        for die in dice_faces:
+            row_components.append(die[row_idx])
+        row_string = DIE_FACE_SEPARATOR.join(row_components)
+        dice_faces_rows.append(row_string)
+
+    width = len(dice_faces_rows[0])
+    diagram_header = " RESULTS ".center(width, "~")
+
+    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
+    return dice_faces_diagram
+
 def parse_input(input_string):
     """Return 'input_string' as int between 1-6
     """
@@ -23,5 +92,6 @@ def roll_dice(num_dice):
 num_dice_input = input("How many dice to roll? [1-6] ")
 num_dice = parse_input(num_dice_input)
 roll_results = roll_dice(num_dice)
-print(roll_results)
+dice_face_diagram = generate_dice_faces_diagram(roll_results)
+print(f"\n{dice_face_diagram}")
 
